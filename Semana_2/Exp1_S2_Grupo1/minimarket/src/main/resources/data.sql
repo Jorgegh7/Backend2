2fec6b8ef6ce60e1b25128818ad5f6e478682756
Jorgegh7 <luzzdeurano@gmail.com>
2026-06-14 10:04:45 -0400

    Semana 4 - Proyecto unificado con pruebas unitarias

--- Semana_2/Exp1_S2_Grupo1/minimarket/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- Roles iniciales del sistema
--- Se insertan automaticamente al iniciar la aplicacion
--- Requiere spring.jpa.defer-datasource-initialization=true en application.properties
-INSERT INTO rol (nombre) VALUES ('ADMIN');
-INSERT INTO rol (nombre) VALUES ('EMPLEADO');
-INSERT INTO rol (nombre) VALUES ('CLIENTE');
-INSERT INTO rol (nombre) VALUES ('GERENTE');
